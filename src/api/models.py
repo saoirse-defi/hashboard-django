330d0aa3444e1b87dcaa9748d877124e05b5dddd
saoirse-defi <saoirsef@yahoo.co.uk>
2025-03-24 18:05:39 +0000

    change name of address model content field

--- src/api/models.py
+++ src/api/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Address(models.Model):
-    address = models.CharField(max_length=200)
+    content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="addresses")
 
     def __str__(self):
-        return self.address
+        return self.content
         
