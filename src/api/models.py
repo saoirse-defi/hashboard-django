e0503157802b7a7e0d6c7326572e00a3cffdec8a
saoirse-defi <saoirsef@yahoo.co.uk>
2025-04-03 20:47:24 +0100

    change name of property to be more legible

--- src/api/models.py
+++ src/api/models.py
@@ -3,21 +3,23 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 
 class Address(models.Model):
-    content = models.CharField(max_length=200)
-    etherscan_data = models.JSONField(null=True, blank=True)
+    address = models.CharField(max_length=200)
+    # etherscan_data = models.JSONField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="addresses")
 
     def __str__(self):
-        if self.etherscan_data:
-            if 'result' in self.etherscan_data:
-                try:
-                    balance = str(self.etherscan_data['result']) #ensure result is a string.
-                    return f"{self.content} (Balance: {balance})"
-                except (TypeError, ValueError) as e:
-                    return f"{self.content} (Balance: Error: {e})"
-            else:
-                return f"{self.content} (Balance: Result key missing)"
-        else:
-            return self.content
+        return self.address
         
+    # def __str__(self):
+    #     if self.etherscan_data:
+    #         if 'result' in self.etherscan_data:
+    #             try:
+    #                 balance = str(self.etherscan_data['result']) #ensure result is a string.
+    #                 return f"{self.content} (Balance: {balance})"
+    #             except (TypeError, ValueError) as e:
+    #                 return f"{self.content} (Balance: Error: {e})"
+    #         else:
+    #             return f"{self.content} (Balance: Result key missing)"
+    #     else:
+    #         return self.content
