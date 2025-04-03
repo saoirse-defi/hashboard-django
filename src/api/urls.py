178340b91a7cab62ee0072cd5b6126fd55674406
saoirse-defi <saoirsef@yahoo.co.uk>
2025-04-03 20:47:53 +0100

    add get user addresses path

--- src/api/urls.py
+++ src/api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # path('user-addresses/', views.get_user_addresses, name='user_addresses'),
     path('eth-balance/', views.get_eth_balance, name='get_eth_balance'),
-    path('save-data/', views.save_data, name='save_data'),
+    path('save-address/', views.save_address, name='save_address'),
+    path('user-addresses/', views.get_user_addresses, name='get_user_addresses'),
 ]
