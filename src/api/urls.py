e3ab6b123f5378347745d7b9ff5bfa6e85f5eb6a
saoirse-defi <saoirsef@yahoo.co.uk>
2025-04-03 11:04:47 +0100

    simplified urls, will add more functionality later

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("addresses/", views.AddressListCreate.as_view(), name="address-list"),
-    path('addresses/<int:address_id>/etherscan/', views.address_etherscan_update),
-    path("addresses/delete/<int:pk>/", views.AddressDelete.as_view(), name="delete-address"),
+    path('eth-balance/', views.get_eth_balance, name='get_eth_balance'),
 ]
